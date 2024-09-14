04c7d1dd4beb4c0ee0b3989583b520ad3f99b6a9
Manoj Shiv <manojshivaprakash10@gmail.com>
2024-09-14 18:03:42 +0530

    Mailing service in Uber App

---
+++ uberApp/src/main/java/com/vestachrono/project/uber/uberApp/services/Implementation/EmailSenderServiceImpl.java
@@ -0,0 +1,51 @@
+package com.vestachrono.project.uber.uberApp.services.Implementation;
+
+import com.vestachrono.project.uber.uberApp.services.EmailSenderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailSenderServiceImpl implements EmailSenderService {
+
+    private final JavaMailSender javaMailSender;
+
+    @Override
+    public void sendEmail(String toEmail, String subject, String body) {
+
+        try{
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+            simpleMailMessage.setTo(toEmail);
+            simpleMailMessage.setSubject(subject);
+            simpleMailMessage.setText(body);
+
+            javaMailSender.send(simpleMailMessage);
+            log.info("Email sent successfully");
+        } catch (Exception exception) {
+            log.error("Cannot send email, {}", exception.getMessage());
+        }
+
+    }
+
+    @Override
+    public void sendEmail(String[] toEmail, String subject, String body) {
+
+        try {
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+            simpleMailMessage.setTo(toEmail);
+            simpleMailMessage.setSubject(subject);
+            simpleMailMessage.setText(body);
+
+            javaMailSender.send(simpleMailMessage);
+            log.info("Email sent successfully");
+        } catch (Exception exception) {
+            log.error("Cannot send email, {}", exception.getMessage());
+        }
+    }
+}
--- uberApp/src/test/java/com/vestachrono/project/uber/uberApp/UberAppApplicationTests.java
+++ uberApp/src/test/java/com/vestachrono/project/uber/uberApp/UberAppApplicationTests.java
@@ -1,13 +1,38 @@
 package com.vestachrono.project.uber.uberApp;
 
+import com.vestachrono.project.uber.uberApp.services.EmailSenderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UberAppApplicationTests {
 
+	@Autowired
+	private EmailSenderService emailSenderService;
+
 	@Test
 	void contextLoads() {
+
+		emailSenderService.sendEmail(
+				"lareki8980@asaud.com",
+				"This is a test email Subject",
+				"This is a test email body");
+
+	}
+
+	@Test
+	void sendEMailToMultipleMailId() {
+
+		String emails[] = {
+				"lareki8980@asaud.com",
+				"manojshivaprakash10@gmail.com",
+				"shivaprakashmanoj@gmail.com"
+		};
+
+		emailSenderService.sendEmail(emails,
+				"Hello from Uber application demo",
+				"This is a test email body");
 	}
 
 }
